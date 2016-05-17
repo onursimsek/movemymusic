91b0c0077d6087a5ed765544a7bf7c390d4d973e
onursimsek <posta@onursimsek.com>
2016-05-17 23:27:15 +0300

    Müzik sağlayıcıları için mixin kullanımı yerine ayrı ayrı Vue sınıfları kullanımı tercih edildi. Deezer ve Spotify için setPlaylist metotları yazıldı.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var globalThat = this;
 var APP_URL = 'http://3m.dev';
 var STORAGE_KEY = 'move-my-music';
 
@@ -10,50 +11,83 @@ var moveMyMusicStorage = {
     }
 };
 
-var deezerMixin = {
+var deezer = new Vue({
+    el: '#deezer',
+    data: {
+        appId: '163985',
+        channel: APP_URL + '/channel.html',
+        scopes: [
+            'basic_access',
+            'email',
+            'manage_library'
+        ],
+        login: {
+            status: false,
+            accessToken: '',
+            response: {}
+        },
+        me: {},
+        playlists: [],
+        tracks: []
+    },
+    ready: function () {
+        this.getLoginStatus();
+    },
     methods: {
-        deezerInit: function () {
+        init: function () {
             DZ.init({
-                appId: '163985',
-                channelUrl: APP_URL + '/channel.html'
+                appId: this.appId,
+                channelUrl: this.channel
             });
         },
-        deezerLogin: function () {
+        getLogin: function () {
+            this.init();
+
             var that = this;
-
-            that.deezerInit();
-
             DZ.login(function (response) {
                 if (!response.status || !response.authResponse) {
-                    console.log('User cancelled login or did not fully authorize.');
-                    that.loginError = 'No access!';
+                    error.setError('User cancelled login or did not fully authorize.', 'alert');
                     return false;
                 }
 
-                that.deezer.status = true;
-                that.deezer.response = response;
-                that.deezer.accessToken = response.authResponse.accessToken;
+                that.login = {
+                    status: true,
+                    accessToken: response.authResponse.accessToken,
+                    response: response
+                };
 
-                moveMyMusicStorage.set(JSON.parse(JSON.stringify(that.$data)));
-            }, {perms: 'basic_access,email'});
+                that.getMe();
+                //moveMyMusicStorage.set(JSON.parse(JSON.stringify({deezer: that.$data})));
+            }, {perms: this.scopes.join(',')});
         },
-        deezerLoginStatus: function () {
-            var that = this;
-            if (that.deezer.status) {
-                that.deezerInit();
+        getLoginStatus: function () {
+            if (this.login.status) {
+                this.init();
 
+                var that = this;
                 DZ.getLoginStatus(function (response) {
                     if (!response.authResponse) {
-                        that.deezer.status = false;
+                        that.login.status = false;
                     }
                 });
             }
         },
-        getDeezerPlaylist: function (type) {
+        getMe: function () {
             var that = this;
+            DZ.api('/user/me', 'GET', function (response) {
+                that.me = {
+                    id: response.id,
+                    username: response.name,
+                    avatar: response.picture_big
+                };
+            });
+        },
+        getPlaylists: function () {
+            var that = this;
+            that.playlists = [];
             DZ.api('user/me/playlists', 'GET', function (response) {
                 response.data.forEach(function (r) {
-                    that[type].playlists.push({
+                    that.playlists.push({
                         id: r.id,
                         title: r.title,
                         picture: r.picture_big,
@@ -61,29 +95,106 @@ var deezerMixin = {
                     });
                 });
             });
+        },
+        setPlaylists: function (title, tracks) {
+            DZ.api('user/me/playlists', 'POST', {title: title}, function (response) {
+                console.log('Playlist created for deezer: ' + response.id);
+                DZ.api('playlist/' + response.id + '/tracks', {songs: tracks}, function (response) {
+                    console.log('Added tracks to playlist');
+                });
+            });
         }
     }
-};
+});
 
-var spotifyMixin = {
+var spotify = new Vue({
+    el: '#spotify',
+    data: {
+        apiUrl: 'https://api.spotify.com/v1',
+        appId: 'b8a1908692314b44852927d11ff15234',
+        callback: APP_URL,
+        scopes: [
+            'user-read-private',
+            'playlist-read-private',
+            'playlist-modify-public',
+            'playlist-modify-private',
+            'user-library-read',
+            'user-library-modify'
+        ],
+        login: {
+            status: false,
+            accessToken: '',
+            refreshToken: '',
+            response: {}
+        },
+        me: {},
+        playlists: [],
+        tracks: []
+    },
+    ready: function () {
+        this.getLoginStatus();
+    },
     methods: {
-        spotifyLogin: function () {
-            var clientId = 'b8a1908692314b44852927d11ff15234';
+        call: function (url, data, callback, method) {
+            var method = method || 'get';
 
+            Vue.http.headers.common['Authorization'] = 'Bearer ' + this.login.accessToken;
+
+            this.$http({
+                url: this.apiUrl + url,
+                method: method,
+                data: data
+            }).then(function (response) {
+                callback(response.data);
+            }, function () {
+                this.refreshToken();
+            });
+        },
+        getLogin: function () {
             var src = 'https://accounts.spotify.com/authorize?';
             var arg = [];
-            arg.push('client_id=' + clientId);
-            arg.push('redirect_uri=' + encodeURIComponent(APP_URL));
+            arg.push('client_id=' + this.appId);
+            arg.push('redirect_uri=' + this.callback);
             arg.push('response_type=token');
-            arg.push('scope=user-library-read');
+            arg.push('scope='.this.scopes.join(','));
 
             document.location = src + arg.join('&');
         },
-        getSpotifyPlaylist: function (type) {
+        getLoginStatus: function () {
+            var args = parseArgs();
+
+            if ('access_token' in args) {
+                this.login = {
+                    status: true,
+                    response: args,
+                    accessToken: args['access_token']
+                };
+
+                this.getMe();
+
+                //this.getRefreshToken();
+
+                //moveMyMusicStorage.set(JSON.parse(JSON.stringify(that.$data)));
+
+                document.location = '/';
+            }
+        },
+        getMe: function () {
             var that = this;
-            this.spotifyCall(this.spotify.api + '/me/playlists', function (playlists) {
+            this.call('/me', {}, function (me) {
+                that.me = {
+                    id: me.id,
+                    username: me.display_name,
+                    avatar: me.images.length ? me.images[0].url : null
+                };
+            });
+        },
+        getPlaylists: function () {
+            var that = this;
+            that.playlists = [];
+            this.call('/me/playlists', {}, function (playlists) {
                 playlists.items.forEach(function (r) {
-                    that[type].playlists.push({
+                    that.playlists.push({
                         id: r.id,
                         title: r.name,
                         picture: r.images[0].url,
@@ -92,21 +203,53 @@ var spotifyMixin = {
                 })
             });
         },
-        spotifyCall: function (url, callback, method) {
-            var method = method || 'get';
+        setPlaylists: function (title, tracks, public) {
+            var public = public || true;
+            var that = this;
+            this.call('/users/' + this.me.id + '/playlists', {name: title, public: public}, function (playlist) {
+                that.call('/users/' + that.me.id + '/playlists/' + playlist.id + '/tracks', {uris: tracks}, function () {
 
-            Vue.http.headers.common['Authorization'] = 'Bearer ' + this.spotify.accessToken;
+                }, 'post');
+            }, 'post');
+        },
+        setRefreshToken: function () {
+            Vue.http.headers.common['Authorization'] = 'Basic ' + btoa(this.appId + 'd7a5703c5e7c4cae87f8aff3fcd1c417');
 
-            this.$http({url: url, method: method}).then(function (response) {
-                callback(response.data);
+            this.$http.post(
+                'https://accounts.spotify.com/api/token',
+                {
+                    grant_type: 'authorization_code',
+                    code: this.login.accessToken,
+                    redirect_uri: this.callback
+                },
+                function (response) {
+                    this.login.accessToken = response.auth_token;
+                    this.login.refreshToken = response.refresh_token;
+                    this.login.response = response;
+                }
+            );
+        },
+        refreshToken: function () {
+            Vue.http.headers.common['Authorization'] = 'Basic ' + btoa(this.appId + 'd7a5703c5e7c4cae87f8aff3fcd1c417');
+
+            this.$http({
+                url: 'https://accounts.spotify.com/api/token',
+                method: 'post',
+                data: {
+                    grant_type: 'authorization_code',
+                    refresh_token: this.login.refreshToken
+                }
+            }).then(function (response) {
+                this.login.accessToken = response.auth_token;
+                this.login.response = response;
             });
         }
     }
-};
+});
 
-new Vue({
+var app = new Vue({
     el: '.moveMyMusicApp',
-    data: Object.assign({
+    data: {
         source: {
             name: 'deezer',
             playlists: [],
@@ -117,47 +260,21 @@ new Vue({
             playlists: [],
             tracks: []
         },
+        tmp: {},
         providers: [
             {text: 'Deezer', value: 'deezer'},
             {text: 'Spotify', value: 'spotify'}
         ],
-        loginError: '',
-        deezer: {
-            status: false,
-            accessToken: '',
-            response: {}
-
-        },
-        spotify: {
-            api: 'https://api.spotify.com/v1',
-            status: false,
-            accessToken: '',
-            response: {}
-        }
-    }, moveMyMusicStorage.get()),
-    mixins: [deezerMixin, spotifyMixin],
-    ready: function () {
-        var that = this;
-        var args = parseArgs();
-
-        if ('access_token' in args) {
-            that.spotify.status = true;
-            that.spotify.response = args;
-            that.spotify.accessToken = args['access_token'];
-
-            moveMyMusicStorage.set(JSON.parse(JSON.stringify(that.$data)));
-        }
-
-        this.deezerLoginStatus();
+        errors: []
     },
     methods: {
         loginStatus: function (provider) {
-            return typeof this[provider].status != 'undefined' ? this[provider].status : false;
+            return typeof globalThat[provider].login.status != 'undefined' ? globalThat[provider].login.status : false;
         },
         login: function (provider) {
             switch (provider) {
                 case 'deezer':
-                    this.deezerLogin();
+                    deezer.getLogin();
                     break;
                 case 'spotify':
                     this.spotifyLogin();
@@ -167,24 +284,81 @@ new Vue({
                     return false;
             }
         },
-        fetchPlaylist: function () {
-            this.getPlaylist(this.source.name, 'source');
-            this.getPlaylist(this.target.name, 'target');
+        sourceChange: function () {
+            this.source.playlists = [];
+            this.source.tracks = [];
         },
-        getPlaylist: function (provider, type) {
+        targetChange: function () {
+            this.target.playlists = [];
+            this.target.tracks = [];
+        },
+        changeDirection: function () {
+            this.tmp = this.source;
+            this.source = this.target;
+            this.target = this.tmp;
+        },
+        fetchPlaylist: function () {
+            this.getPlaylists(this.source.name, 'source');
+            this.getPlaylists(this.target.name, 'target');
+        },
+        getPlaylists: function (provider, type) {
             switch (provider) {
                 case 'deezer':
-                    this.getDeezerPlaylist(type);
+                    this.getDeezerPlaylists();
                     break;
                 case 'spotify':
-                    this.getSpotifyPlaylist(type);
+                    this.getSpotifyPlaylists();
                     break;
                 default:
                     return false;
             }
+        },
+        movePlaylist: function (playlist) {
+
         }
     }
 });
+
+var error = new Vue({
+    data: {
+        errors: []
+    },
+    methods: {
+        getError: function () {
+            return this.errors;
+        },
+        setError: function (text, type) {
+            this.errors.push({
+                text: text,
+                type: type
+            });
+        }
+    }
+});
+
+function Me() {
+    this.id = '';
+    this.username = '';
+    this.avatar = '';
+    this.getId = function () {
+        return this.id;
+    };
+    this.setId = function (id) {
+        this.id = id;
+    };
+    this.getUsername = function () {
+        return this.username;
+    };
+    this.setUsername = function (username) {
+        this.username = username;
+    };
+    this.getAvatar = function () {
+        return this.avatar;
+    };
+    this.setAvatar = function (avatar) {
+        this.avatar = avatar;
+    }
+}
 
 function parseArgs() {
     var hash = location.hash.replace(/#/g, '');
